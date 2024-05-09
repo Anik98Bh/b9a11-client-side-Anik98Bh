60978e9986b0a2f5bd79c131ee4a942dbf95cf20
Anik98B <anikbhowmik0188@gmail.com>
2024-05-10 02:19:06 +0600

    create register page

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import {
 import router from './routes/router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
 )
